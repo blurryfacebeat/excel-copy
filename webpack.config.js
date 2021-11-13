b626162d2c04f3433261815e63964162a63c33c5
blurryfacebeat <blurryfacebeat@gmail.com>
2021-11-13 18:16:09 +0700

    Refactor webpack and package

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,35 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: './index.js',
+const isDevMode = process.env.NODE_ENV !== 'production';
+
+const filename = (extension) =>
+  isDevMode ? `bundle.[fullhash].${extension}` : `bundle.${extension}`;
+
+const config = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: ['@babel/polyfill', path.resolve(__dirname, './src/index.js')],
   output: {
-    filename: 'bundle.[fullhash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, './dist'),
+    filename: filename('js')
   },
   resolve: {
-    extensions: ['.js'],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core')
+      '@': path.resolve(__dirname, './src')
     }
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      template: 'index.html'
-    }),
-    new CopyPlugin({
-      patterns:
-        [
-          {
-            from: path.resolve(__dirname, 'src/favicon.ico'),
-            to: path.resolve(__dirname, 'dist')
-          }
-        ]
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'bundle.[fullhash].css'
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
+      // ** TYPESCRIPT ** //
+      // {
+      //   test: /\.ts$/,
+      //   exclude: /node_modules/,
+      //   use: ['ts-loader']
+      // },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
@@ -56,7 +39,61 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'New Project',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+      minify: {
+        removeComments: !isDevMode,
+        collapseWhitespace: !isDevMode
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/favicon.ico'),
+          to: path.resolve(__dirname, './dist')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+if (isDevMode) {
+  config.devServer = {
+    historyApiFallback: true,
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080
+  };
 }
+
+module.exports = config;
